e069ae2135f4944ae6ceefa096a910de7bf726c3
imemilyhuang <emilyhuang405@gmail.com>
2024-04-22 17:10:12 -0500

    team member padding

--- src/components/team/TeamMember.js
+++ src/components/team/TeamMember.js
@@ -19,7 +19,7 @@ const TeamMember = ({data}) => {
   }, [data.imagePath])
   
   return <a href={data?.link} target="_blank" rel="noopener noreferrer">
-    <div className='pad2 t-card'>
+    <div className='pad t-card'>
       <img className='execPic marginb' src={image} alt={data.name} />
       <h4 className='padb center-text'>{data.name}</h4>
       <p className='center-text purple-text'>{data.title}</p>
